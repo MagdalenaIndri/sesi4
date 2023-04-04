821d6fc0b223b61802c3e8dbec71c13985b1a040
MagdalenaIndri <127809418+MagdalenaIndri@users.noreply.github.com>
2023-04-04 12:27:33 +0800

    Add files via upload

---
+++ sesi4/latihan03.php
@@ -0,0 +1,44 @@
+<?php
+    $path ="Upgambar/";
+    if(isset($_POST["uploadfile"])){
+        if($_FILES["flGAMBAR"]["error"]==0){
+            $flname = $path . basename($_FILES["flGAMBAR"]["name"]);
+            move_uploaded_file($_FILES["flGAMBAR"]["tmp_name"], $flname);
+        }
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload File</title>
+</head>
+<body>
+<?php
+if(isset($_POST["uploadfile"])){
+    echo "<img src='" .$flname."'>";;
+}
+
+?>
+
+
+    <form action="latihan03.php" method="POST" enctype="multipart/form-data">
+
+    <div>
+        Upload Gambar
+        <input type="File" name="flGAMBAR">
+    </div>
+
+    <div>
+        <input type="hidden" name="uploadfile" value="good">
+        <button type ="submit">Upload gambar</button>
+</form>
+
+
+</body>
+</html>
